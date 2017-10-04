5d4461c266d2c05ff2362b3fb6c94e4432a5236d
Emilio Moreira de Andrade <Duilio_Andrade@hotmail.com>
2017-10-03 22:40:36 -0300

    Explicacao Rotas, Controllers e View

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -16,13 +17,31 @@ namespace Vidly.Controllers
             //return View(movie);
             //return Content("Hello World");
             //return HttpNotFound();
-            //retur new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //retur new EmptyResult();  
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });            
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMoviesViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id)
         {
             return Content("id = " + id);
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
