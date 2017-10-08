60dbb369d98a136a9b7adaf27372f47ef93978cf
Emilio Moreira de Andrade <Duilio_Andrade@hotmail.com>
2017-10-08 15:57:47 -0300

    Correcao Exercicio

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -11,46 +11,31 @@ namespace Vidly.Controllers
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var customers = GetCustomers();
+
+            return View(customers);
         }
 
-        public ActionResult Random()
-        {
-            var customers = new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Smith"},
-                new Customer {Id = 2, Name = "Mary Williams"}
-            };
-
-            var viewModel = new RandomCustomersViewModel
-            {
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
-
-        [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {
-            var viewModel = new CustomerViewModel();
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
 
-            if (id == 1)
-            {
-                viewModel.Customer = new Customer { Id = 1, Name = "John Smith" };
-                return View(viewModel);
-            }
-            else if(id == 2)
-            {
-                viewModel.Customer = new Customer { Id = 2, Name = "Mary Williams" };
-                return View(viewModel); ;
-            }
-            else
+            if (customer == null)
             {
                 return HttpNotFound();
             }
+                
+            return View(customer);
+        }
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id = 1, Name = "João Souza"},
+                new Customer {Id = 2, Name = "Maria Madalena"}
+            };
         }
     }
 }
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -11,57 +11,32 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies/Random
-        public ActionResult Random()
+        public ViewResult Index()
         {
-            //return View(movie);
-            //return Content("Hello World");
-            //return HttpNotFound();
-            //retur new EmptyResult();  
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });  
+            var movies = GetMovies();
 
-            var movies = new List<Movie>
-            {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
-            };
-            var viewModel = new RandomMoviesViewModel
-            {
-                Movies = movies
-            };
-
-            return View(viewModel);
+            return View(movies);
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id = " + id);
-        }
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
-
-        [Route("movies/details/{id}")]
         public ActionResult Details (int id)
         {
-            var viewModel = new MovieViewModel();
-            
-            if(id == 1)
-            {
-                viewModel.Movie = new Movie { Id = 1, Name = "Shrek" };
-                return View(viewModel);
-            }
-            else if(id == 2)
-            {
-                viewModel.Movie = new Movie { Id = 2, Name = "Wall-e" };
-                return View(viewModel);
-            }
-            else
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
             {
                 return HttpNotFound();
             }
+
+            return View(movie);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek !"},
+                new Movie {Id = 2, Name = "Wall-e"}
+            };
         }
     }
 }
